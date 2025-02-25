ec4686f06ade35be933871557ed37d082609dede
marco <marco.frautschi@bluewin.ch>
2025-02-25 17:22:23 +0100

    feat: changed to angular sortable tables

--- angular-techradar/src/app/list-technology/list-technology.component.ts
+++ angular-techradar/src/app/list-technology/list-technology.component.ts
@@ -1,14 +1,40 @@
 import {TechnologyService} from '../technology.service';
 import {Technology} from '../Technology';
 import {MatCardModule} from '@angular/material/card';
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {Status} from '../status';
 import {Category} from '../category';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from '@angular/material/table';
+import {MatSort, MatSortHeader, Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-list-technology',
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatTable,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatColumnDef,
+    MatCell,
+    MatHeaderRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatRow,
+    MatSortHeader,
+    MatSort
   ],
   templateUrl: './list-technology.component.html',
   standalone: true,
@@ -17,21 +43,80 @@ import {Category} from '../category';
 export class ListTechnologyComponent {
   technologies: Technology[] = [];
 
+  displayedColumns: string[] = ['id', 'name', 'category', 'ring', 'techDescription', 'classDescription', 'status', 'creationDate', 'publicationDate'];
+  techniques: Technology[] = [];
+  tools: Technology[] = [];
+  platforms: Technology[] = [];
+  languagesAndFrameworks: Technology[] = [];
+
+  dataSourceTechniques = new MatTableDataSource(this.techniques);
+  dataSourceTools = new MatTableDataSource(this.tools);
+  dataSourcePlatforms = new MatTableDataSource(this.platforms);
+  dataSourceLanguagesAndFrameworks = new MatTableDataSource(this.languagesAndFrameworks);
+
   constructor(private readonly technologyService: TechnologyService) {
   }
 
   ngOnInit() {
-    this.getTechnologyREST();
+    this.getTechnologyREST().then(r => console.log('got data'));
   }
 
   getTechnologies() {
-    this.technologyService.getTechnologies().subscribe(techs => this.technologies = techs);
+    this.technologyService.getTechnologies().subscribe(techs => this.splitTechnologies(techs));
   }
 
-  async getTechnologyREST(){
-    this.technologies = await this.technologyService.getTechnologiesREST();
+  splitTechnologies(techs: Technology[]) {
+    techs.filter(t => t.status === Status.Published).forEach(tech => {
+      switch (tech.category) {
+        case Category.Techniques:
+          this.techniques.push(tech);
+          break;
+        case Category.Tools:
+          this.tools.push(tech);
+          break;
+        case Category.Platforms:
+          this.platforms.push(tech);
+          break;
+        case Category.LanguagesAndFrameworks:
+          this.languagesAndFrameworks.push(tech);
+          break;
+      }
+    });
+
+    this.dataSourceTechniques.data = this.techniques;
+    this.dataSourceTools.data = this.tools;
+    this.dataSourcePlatforms.data = this.platforms;
+    this.dataSourceLanguagesAndFrameworks.data = this.languagesAndFrameworks;
+    console.table(this.techniques);
+    console.table(this.tools);
+    console.table(this.platforms);
+    console.table(this.languagesAndFrameworks);
+  }
+
+  async getTechnologyREST() {
+    this.splitTechnologies(await this.technologyService.getTechnologiesREST());
   }
 
   protected readonly Status = Status;
   protected readonly Category = Category;
+
+  @ViewChild('sortTechniques') sortTechniques: MatSort = {} as MatSort;
+  @ViewChild('sortTools') sortTools: MatSort = {} as MatSort;
+  @ViewChild('sortPlatforms') sortPlatforms: MatSort = {} as MatSort;
+  @ViewChild('sortLanguagesAndFrameworks') sortLanguagesAndFrameworks: MatSort = {} as MatSort;
+
+  ngAfterViewInit() {
+    this.dataSourceTechniques.sort = this.sortTechniques;
+    this.dataSourceTools.sort = this.sortTools;
+    this.dataSourcePlatforms.sort = this.sortPlatforms;
+    this.dataSourceLanguagesAndFrameworks.sort = this.sortLanguagesAndFrameworks;
+  }
+
+  announceSort(sortState: Sort) {
+    if (sortState.direction) {
+      console.log(`Sorted ${sortState.direction}ending`);
+    } else {
+      console.log('Sorting cleared');
+    }
+  }
 }
