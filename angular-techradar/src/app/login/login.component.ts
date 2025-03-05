8ca239801bb3e75369aceb0ec063b17e2c200c1a
marco <marco.frautschi@bluewin.ch>
2025-03-05 15:32:09 +0100

    feat: implement login ui with angular material

--- angular-techradar/src/app/login/login.component.ts
+++ angular-techradar/src/app/login/login.component.ts
@@ -1,30 +1,41 @@
 import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../auth.service';
 import {firstValueFrom} from 'rxjs';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   imports: [
-    FormsModule
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatButton
   ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  username: string | undefined;
-  password: string | undefined;
 
   constructor(private readonly http: HttpClient, private readonly router: Router, private readonly authService: AuthService,) {
   }
 
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required)
+  });
+
   async onSubmit() {
     const loginData = {
-      username: this.username,
-      password: this.password
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
     }
     try {
       const response = await firstValueFrom(
@@ -36,7 +47,7 @@ export class LoginComponent {
 
       if (response.success) {
         this.authService.saveToken(response.token);
-        this.router.navigate(['/techradar']);
+        await this.router.navigate(['/techradar']);
       } else {
         alert('Login failed: ' + response.message);
       }
